9cb5210c1da94bf874cbd22063229c57b9c32613
piggybackcoder1 <swainrakeshkumar60@gmail.com>
2022-06-18 20:15:22 +0530

    login sand signup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Signin from './components/SignIn/Signin';
+import Signup from './components/SignUp/Signup';
+
+function App() {
+  return (
+    <div className="App">
+        <Signin></Signin><br></br>
+        <Signup></Signup>
+        
+    </div>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,33 @@
-require("dotenv").config();
 const express = require("express");
+const dotenv = require('dotenv')
+const userRouter = require('./routes/auth')
+const products = require("./model/products");
 const app = express();
-const cors = require("cors");
-const connection = require("./db");
-const userRoutes = require("./routes/users");
-const authRoutes = require("./routes/auth");
-
-// database connection
-connection();
-
-// middlewares
+require("dotenv").config();
+const connect = require('./storage/db')
+const cors = require('cors')
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(cors());
 
-// routes
-app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+    })
+    );
+    
+app.use('/user',userRouter)
+app.get("/products", async (req, res) => {
+    const Products = await products.find();
+    
+  
+    res.status(201).send(Products);
+  });
 
-const port = process.env.PORT || 8080;
-app.listen(port, console.log(`Listening on port ${port}...`));
+app.get('/',(req,res)=>{
+    res.send("Server started")
+})
+app.listen(process.env.PORT||8080,async()=>{
+    await connect;
+    console.log("connected to db")
+    console.log("server started")
+})
